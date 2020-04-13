20982ab56e78c0118831d98a3829e665ea17d9b6
ankarenver <1996ankarjian@gmail.com>
2020-04-12 21:52:12 -0400

    test 1 to 12 finished

--- src/test/java/com/automation/pages/Activities/CalendarEvents.java
+++ src/test/java/com/automation/pages/Activities/CalendarEvents.java
@@ -2,10 +2,14 @@ package com.automation.pages.Activities;
 
 import com.automation.pages.PageBase;
 import com.automation.utilities.BrowserUtilities;
+import com.automation.utilities.DateTimeUtilities;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -28,6 +32,50 @@ public class CalendarEvents extends PageBase {
     @FindBy(xpath = "//thead[@class='grid-header']//th[2]")
     private WebElement tableHeaderName;
 
+    @FindBy(xpath = "//a[@class='btn-success btn dropdown-toggle']//span")
+    private WebElement expandOfSaveAndClose;
+
+    @FindBy(xpath = "//a[@title='Cancel']")
+    private WebElement cancelBtn;
+
+    @FindBy(xpath = "(//input[@placeholder ='time'])[1]")
+    private WebElement time1;
+
+    @FindBy(xpath = "(//input[@placeholder ='time'])[2]")
+    private WebElement time2;
+
+    @FindBy(name = "oro_calendar_event_form[allDay]")
+    public WebElement allDay;
+
+    @FindBy(xpath = "(//input[@placeholder='Choose a date'])[1]")
+    private WebElement startDate;
+
+    @FindBy(xpath = "(//input[@placeholder='Choose a date'])[2]")
+    private WebElement endDate;
+
+    @FindBy(xpath = "//input[@data-name=\"recurrence-repeat\"]")
+    public WebElement repeatBtn;
+
+    @FindBy(xpath = "//label[@class='fields-row']//input[@type='radio'and @checked='checked']")
+    public WebElement repeatEveryRadio;
+
+    @FindBy(xpath = "//span[contains(text(),'Never')]/..//input")
+    public WebElement endsRadio;
+
+    @FindBy(xpath = "//div[@class=\"control-group recurrence-summary alert-info\"]")
+    public WebElement messageUnderSummary;
+
+    @FindBy(xpath = "//span[contains(text(),'After')]/..//input[@type='radio']")
+    public WebElement afterRadio;
+
+    @FindBy(xpath = "//span[contains(text(),'After')]/..//input[@type='text']")
+    public WebElement afterText;
+
+    @FindBy(xpath = "//span[text()='M']/..//input")
+    public WebElement Monday;
+
+    @FindBy(xpath = "//span[text()='F']/..//input")
+    public WebElement Friday;
 
     public void hoverOverThreeDots(){
         BrowserUtilities.waitForPageToLoad(10);
@@ -55,6 +103,91 @@ public class CalendarEvents extends PageBase {
     public String getTableHeaderName(){
         return tableHeaderName.getText();
     }
+
+    public void clickOnCreateCalendarEventBtn(){
+        createCalendarBtn.click();
+    }
+
+    public void getNameOfTheDropdown(){
+        BrowserUtilities.waitForPageToLoad(10);
+        BrowserUtilities.wait(5);
+        driver.findElement(By.xpath("(//div[@class='controls']//input[@type='text'])[3]")).sendKeys("sadasad");
+        BrowserUtilities.wait(1);
+        List<WebElement> allDropdownElements = driver.findElements(By.xpath("//li//button[@type='submit']"));
+//        return BrowserUtilities.getTextFromWebElements(allDropdownElements);
+        allDropdownElements.forEach(each-> System.out.println(each.getText().trim()));
+    }
+
+    public void clickOnCancelBtn(){
+        BrowserUtilities.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.elementToBeClickable(cancelBtn));
+        cancelBtn.click();
+    }
+    public boolean isAllCalendarEventsDisplayed(){
+        return driver.findElement(By.xpath("//h1[@class='oro-subtitle']")).isDisplayed();
+    }
+
+    public long getTimeDifferences(){
+        BrowserUtilities.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.visibilityOf(time1));
+        String time1Full = time1.getAttribute("value");
+        String time2Full = time2.getAttribute("value");
+        return DateTimeUtilities.getTimeDifference(time1Full, time2Full, "h:m a");
+    }
+
+    public void selectTime(String time){
+        BrowserUtilities.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.visibilityOf(time1));
+        time1.clear();
+        time1.sendKeys(time, Keys.ENTER);
+    }
+    public String getTime1(){
+        return time1.getAttribute("value");
+    }
+    public String getTime2(){
+        return time2.getAttribute("value");
+    }
+
+    public void clickOnAllDayEvent(){
+        BrowserUtilities.waitForPageToLoad(10);
+        BrowserUtilities.wait(5);
+        allDay.click();
+        BrowserUtilities.wait(2);
+    }
+
+    public boolean isTimesAreDisplayed(){
+        return time1.isDisplayed()&&time2.isDisplayed();
+    }
+
+    public boolean isDatesAreDisplayed(){
+        return  startDate.isDisplayed()&&endDate.isDisplayed();
+    }
+
+    public void clickOnRepeatBtn(){
+        BrowserUtilities.waitForPageToLoad(10);
+        BrowserUtilities.wait(3);
+        repeatBtn.click();
+        BrowserUtilities.wait(2);
+    }
+
+    public String getDefaultOption(){
+        Select a = new Select(driver.findElement(By.xpath("//select[@data-name=\"recurrence-repeats\"]")));
+        return a.getFirstSelectedOption().getText();
+    }
+
+    public List<String> getAllDropdown(){
+        Select a = new Select(driver.findElement(By.xpath("//select[@data-name=\"recurrence-repeats\"]")));
+        return BrowserUtilities.getTextFromWebElements(a.getOptions());
+    }
+
+    public void clickAfterAndEnterValue(String str){
+        afterRadio.click();
+        afterText.sendKeys(str,Keys.ENTER);
+    }
+
+
+
+
 
 
 
--- src/test/java/com/automation/tests/Tests.java
+++ src/test/java/com/automation/tests/Tests.java
@@ -2,6 +2,10 @@ package com.automation.tests;
 
 import com.automation.pages.Activities.CalendarEvents;
 import com.automation.pages.LoginPage;
+import com.automation.utilities.BrowserUtilities;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,7 +13,7 @@ public class Tests extends TestBase{
 
     @Test
     public void test_01(){
-        extentTest = extentReports.createTest("first test");
+        extentTest = extentReports.createTest("test#1");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
         CalendarEvents a = new CalendarEvents();
@@ -18,23 +22,165 @@ public class Tests extends TestBase{
         Assert.assertTrue(a.isIconVisible("View"));
         Assert.assertTrue(a.isIconVisible("Edit"));
         Assert.assertTrue(a.isIconVisible("Delete"));
-        extentTest.pass("first test pass!!");
+        extentTest.pass("test#1 pass!!");
     }
 
     @Test
     public void test_02(){
-        extentTest = extentReports.createTest("second test");
+        extentTest = extentReports.createTest("test#2 test");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
         CalendarEvents a = new CalendarEvents();
         a.navigateTo("Activities","Calendar Events");
         a.deselectAllOptionsInGrid();
         Assert.assertEquals(a.getTableHeaderName(),"TITLE");
-        extentTest.pass("first test pass!!");
+        extentTest.pass("test#2 pass!!");
+    }
+
+    @Test(description = "not done yet")
+    public void test_03(){
+        extentTest = extentReports.createTest("test#3 test");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.getNameOfTheDropdown();
+        extentTest.pass("test#3 pass!!");
     }
 
     @Test
-    public void test_03(){
-
+    public void test_04(){
+        extentTest = extentReports.createTest("test#4 test");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.clickOnCancelBtn();
+        Assert.assertTrue(a.isAllCalendarEventsDisplayed());
+        extentTest.pass("test#4 pass!!");
     }
+
+    @Test
+    public void test_05(){
+        extentTest = extentReports.createTest("test#5 test");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        Assert.assertEquals(a.getTimeDifferences(),1);
+        extentTest.pass("test#5 pass!!");
+    }
+
+    @Test
+    public void test_06(){
+        extentTest = extentReports.createTest("test#6 test");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.selectTime("9:00 PM");
+        Assert.assertEquals(a.getTime2(),"10:00 PM");
+        extentTest.pass("test#6 pass!!");
+    }
+
+    @Test
+    public void test_07(){
+        extentTest = extentReports.createTest("test#7 test");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.clickOnAllDayEvent();
+        Assert.assertTrue(a.allDay.isSelected());
+        Assert.assertFalse(a.isTimesAreDisplayed());
+        Assert.assertTrue(a.isDatesAreDisplayed());
+        extentTest.pass("test#7 pass!!");
+    }
+
+    @Test
+    public void test_08(){
+        extentTest = extentReports.createTest("test#8 test");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.clickOnRepeatBtn();
+        Assert.assertTrue(a.repeatBtn.isSelected());
+        Assert.assertEquals(a.getDefaultOption(),"Daily");
+        Assert.assertNotEquals(a.getAllDropdown(),null);
+        extentTest.pass("test#8 pass!!");
+    }
+
+    @Test
+    public void test_09(){
+        extentTest = extentReports.createTest("test#9");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.clickOnRepeatBtn();
+        Assert.assertTrue(a.repeatBtn.isSelected());
+        Assert.assertTrue(a.repeatEveryRadio.isSelected());
+        Assert.assertTrue(a.endsRadio.isSelected());
+        Assert.assertTrue(a.messageUnderSummary.getText().contains("Daily every 1 day"));
+        extentTest.pass("test#9 pass!!");
+    }
+
+    @Test
+    public void test_10(){
+        extentTest = extentReports.createTest("test#10");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.clickOnRepeatBtn();
+        a.clickAfterAndEnterValue("10");
+        Assert.assertTrue(a.messageUnderSummary.getText().contains("Daily every 1 day, end after 10 occurrences"));
+        extentTest.pass("test#10 pass!!");
+    }
+
+    @Test
+    public void test_11(){
+        extentTest = extentReports.createTest("test#11");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.clickOnRepeatBtn();
+        driver.findElement(By.xpath("//span[contains(text(),'By')]/..//input")).click();
+        BrowserUtilities.wait(2);
+        driver.findElement(By.xpath("//span[text()='By']/../..//div//input[2]")).sendKeys("Nov 21, 2021", Keys.ENTER);
+        Assert.assertTrue(a.messageUnderSummary.getText().contains("Daily every 1 day, end by Nov 21, 2021"));
+        extentTest.pass("test#11 pass!!");
+    }
+    @Test
+    public void test_12(){
+        extentTest = extentReports.createTest("test#12");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        CalendarEvents a = new CalendarEvents();
+        a.navigateTo("Activities","Calendar Events");
+        a.clickOnCreateCalendarEventBtn();
+        a.clickOnRepeatBtn();
+        Select repeatsDropdown = new Select(driver.findElement(By.xpath("//select[@data-name=\"recurrence-repeats\"]")));
+        repeatsDropdown.selectByVisibleText("Weekly");
+        a.Monday.click();
+        a.Friday.click();
+        Assert.assertTrue(a.Monday.isSelected()&&a.Friday.isSelected());
+        Assert.assertTrue(a.messageUnderSummary.getText().contains("Weekly every 1 week on Monday, Friday"));
+        extentTest.pass("test#12 pass!!");
+    }
+
+
+
+
 }
